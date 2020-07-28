adb5c820301990fc5ae25abc2c99cfc6e4221c46
Prescott Murphy <prurph@gmail.com>
2020-07-28 18:21:23 -0400

    Implement Parser for basic grammar

---
+++ src/tt/presco/lox/Parser.java
@@ -0,0 +1,153 @@
+package tt.presco.lox;
+
+import static tt.presco.lox.TokenType.*;
+
+import java.util.List;
+
+public class Parser {
+  private final List<Token> tokens;
+  private int current = 0;
+
+  Parser(List<Token> tokens) {
+    this.tokens = tokens;
+  }
+
+  // Grammar: lowest precedence to highest. Each rule matches expressions at its precedence level or
+  // higher (e.g. expression matches equality, comparison, ...)
+  //
+  // expression     → equality
+  // equality       → comparison ( ( "!=" | "==" ) comparison )* ;
+  // comparison     → addition ( ( ">" | ">=" | "<" | "<=" ) addition )* ;
+  // addition       → multiplication ( ( "+" | "-" ) multiplication )* ;
+  // multiplication → unary ( "*" | "/" ( unary ) )* ;
+  // unary          → ( "!" | "-" ) unary | primary ;
+  // primary        → NUMBER | STRING | "false" | "true" | "nil" | "(" expression ")" ;
+
+  private Expr expression() {
+    return equality();
+  }
+
+  private Expr equality() {
+    Expr expr = comparison();
+
+    // If we don't encounter any equality operators, equality() effectively calls and returns
+    // comparison(), thus equality matches an equality operator or anything of higher precedence.
+    while (match(BANG_EQUAL, EQUAL_EQUAL)) {
+      Token operator = previous();
+      Expr right = comparison();
+      expr = new Expr.Binary(expr, operator, right);
+    }
+
+    return expr;
+  }
+
+  private Expr comparison() {
+    Expr expr = addition();
+
+    while (match(GREATER, GREATER_EQUAL, LESS, LESS_EQUAL)) {
+      Token operator = previous();
+      Expr right = addition();
+      expr = new Expr.Binary(expr, operator, right);
+    }
+
+    return expr;
+  }
+
+  private Expr addition() {
+    Expr expr = multiplication();
+
+    while (match(PLUS, MINUS)) {
+      Token operator = previous();
+      Expr right = multiplication();
+      expr = new Expr.Binary(expr, operator, right);
+    }
+
+    return expr;
+  }
+
+  private Expr multiplication() {
+    Expr expr = unary();
+
+    while (match(SLASH, STAR)) {
+      Token operator = previous();
+      Expr right = unary();
+      expr = new Expr.Binary(expr, operator, right);
+    }
+
+    return expr;
+  }
+
+  private Expr unary() {
+    if (match(BANG, MINUS)) {
+      Token operator = previous();
+      Expr right = unary();
+      return new Expr.Unary(operator, right);
+    }
+
+    return primary();
+  }
+
+  private Expr primary() {
+    if (match(FALSE)) return new Expr.Literal(false);
+    if (match(TRUE)) return new Expr.Literal(true);
+    if (match(NIL)) return new Expr.Literal(null);
+
+    if (match(NUMBER, STRING)) {
+      return new Expr.Literal(previous().literal);
+    }
+
+    if (match(LEFT_PAREN)) {
+      Expr expr = expression();
+      consume(RIGHT_PAREN, "Expect ')' after expression.");
+      return new Expr.Grouping(expr);
+    }
+
+    throw error(peek(), "Expect expression.");
+  }
+
+  private boolean match(TokenType... types) {
+    for (TokenType type : types) {
+      if (check(type)) {
+        advance();
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  private Token consume(TokenType type, String message) {
+    if (check(type)) return advance();
+
+    throw error(peek(), message);
+  }
+
+  private boolean check(TokenType type) {
+    if (isAtEnd()) return false;
+    return peek().type == type;
+  }
+
+  private Token advance() {
+    if (!isAtEnd()) current++;
+    return previous();
+  }
+
+  private boolean isAtEnd() {
+    return peek().type == EOF;
+  }
+
+  private Token peek() {
+    return tokens.get(current);
+  }
+
+  private Token previous() {
+    return tokens.get(current - 1);
+  }
+
+  private ParseError error(Token token, String message) {
+    Lox.error(token, message);
+    return new ParseError();
+  }
+
+  private static class ParseError extends RuntimeException {}
+}
